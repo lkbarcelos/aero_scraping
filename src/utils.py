f099dfee32e148f8783bebd6ba936b86675257ef
Leandro Klem <leandrok.barcelos@gmail.com>
2024-12-13 20:15:00 -0300

    Criação de função random_wait

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,16 @@
 # Funções Úteis
+import time
+import random
+
+
+def random_wait(min_time, max_time):
+    """
+    Aguarda um tempo aleatório entre min_time e max_time segundos.
+    """
+    wait_time = random.uniform(min_time, max_time)
+    print(f"Aguardando {wait_time:.2f} segundos...")
+    time.sleep(wait_time)
+
+
+
+
