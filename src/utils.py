92643afc0950651a44799b18f5ce2edc1a03663b
Leandro Klem <leandrok.barcelos@gmail.com>
2024-12-14 21:42:53 -0300

    feat: adicionar função click_random_position ao módulo utils

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 # Funções Úteis
 import time
 import random
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 def random_wait(min_time, max_time):
@@ -8,9 +9,27 @@ def random_wait(min_time, max_time):
     Aguarda um tempo aleatório entre min_time e max_time segundos.
     """
     wait_time = random.uniform(min_time, max_time)
-    print(f"Aguardando {wait_time:.2f} segundos...")
+    
     time.sleep(wait_time)
 
-
-
-
+def click_random_position(driver, element, min_wait=0.2, max_wait=1.1):
+    """
+    Move o mouse para uma posição aleatória dentro do elemento, aguarda um tempo aleatório e clica nele.
+    """
+    # Obtém o tamanho do elemento
+    width = element.size['width']
+    height = element.size['height']
+    
+    # Gera uma posição aleatória dentro do elemento
+    random_x = random.randint(0, width - 1)
+    random_y = random.randint(0, height - 1)
+    
+    # Move o mouse para a posição aleatória
+    actions = ActionChains(driver)
+    actions.move_to_element_with_offset(element, random_x, random_y)
+    
+    # Aguarda um tempo aleatório
+    random_wait(min_wait, max_wait)
+    
+    # Clica no elemento
+    actions.click().perform()
